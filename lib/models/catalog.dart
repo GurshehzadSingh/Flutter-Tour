3f6f10d6d69711eee838a98cc6f280b93cfd5bf0
Gurshehzad Singh <singhgurshehzad@gmail.com>
2021-06-08 21:47:20 +0530

    model for products created

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String  image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products=[
+  Item(
+    id: "001",
+    name: "Samsung M31 A",
+    desc: "A Samsung Mobile working on Android with 6GB RAM.",
+    price: 17000,
+    color: "#33505a",
+    image: "https://static.digit.in/default/db80c6f12355307b593c0af60ad5403adbd2ab06.jpeg?tr=1200"
+  )
+];
