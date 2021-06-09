e6bcce105ec01857df46b4a72c977ea8033f29c3
Gurshehzad Singh <singhgurshehzad@gmail.com>
2021-06-09 15:04:37 +0530

    day 12 completed with list view and dummy list

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel{
+  static final items=[
+    Item(
+    id: 1,
+    name: "Samsung M31 A",
+    desc: "A Samsung Mobile working on Android with 6GB RAM.",
+    price: 17000,
+    color: "#33505a",
+    image: "https://static.digit.in/default/db80c6f12355307b593c0af60ad5403adbd2ab06.jpeg?tr=1200"
+  )
+  ];
+}
+
 class Item{
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -10,12 +23,5 @@ class Item{
 }
 
 final products=[
-  Item(
-    id: "001",
-    name: "Samsung M31 A",
-    desc: "A Samsung Mobile working on Android with 6GB RAM.",
-    price: 17000,
-    color: "#33505a",
-    image: "https://static.digit.in/default/db80c6f12355307b593c0af60ad5403adbd2ab06.jpeg?tr=1200"
-  )
+  
 ];
